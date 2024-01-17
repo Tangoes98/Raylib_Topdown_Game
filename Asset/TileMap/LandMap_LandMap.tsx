f71d6c796b84b9797c6bed17051f25779559c51c
Tangoes98 <cyriltan985@gmail.com>
2024-01-16 22:06:07 -0500

    Add Basic game assets

---
+++ Asset/TileMap/LandMap_LandMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="LandMap" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="../mystic_woods_free_2.1/sprites/tilesets/plains.png" width="96" height="192"/>
+</tileset>
